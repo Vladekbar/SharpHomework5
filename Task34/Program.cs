6e89eb4e20b24f11bd3fe16fc7a8ccee652e664b
Uladzimir Baranouski <uladbar@gmail.com>
2023-07-19 22:47:20 +0200

    All are added

---
+++ Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] NewRandomArray(int size, int beginValue, int endValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(beginValue, endValue);
+    return array;
+}
+
+int EvenSearch(int[] array)
+{   
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = NewRandomArray(4, 100, 1000); 
+int count = EvenSearch(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]" + " -> " + count);
---
+++ Task36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] NewRandomArray(int size, int beginValue, int endValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(beginValue, endValue);
+    return array;
+}
+
+int NotEvenSearchAndSum(int[] array)
+{   
+    int sumOfNotEven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sumOfNotEven = sumOfNotEven + array[i];
+        }
+    }
+    return sumOfNotEven;
+}
+
+int[] array = NewRandomArray(4, -9, 9);
+Console.WriteLine("[" + string.Join(", ", array) + "]" + " -> " + NotEvenSearchAndSum(array));
